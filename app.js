7d8143cd48f5d9c8bcdd7ed07a5dbf6878addb9e
s-moreau00 <moreau.sylvain6969@gmail.com>
2024-01-28 19:49:28 +0100

    exo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require('express');
+
+const app = express();
+require('dotenv').config()
+
+
+const port=process.env.SERVER_PORT;
+let name=process.env.NAME
+let city=process.env.CITY
+let langage=process.env.LANGAGE
+
+app.get("/", (req, res) => {
+    res.send("hello :)");
+})
+
+app.listen(port, () => {
+    console.log(`I am ${name} from ${city} and i'm learning ${langage} ! :)`)
+})
